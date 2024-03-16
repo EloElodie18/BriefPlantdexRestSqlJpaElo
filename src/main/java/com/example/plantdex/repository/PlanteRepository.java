fa8a2e2521de9c988ef9ecf713c3e959043f8a2b
EloElodie18 <elodierichard@yahoo.fr>
2024-03-16 15:39:15 +0100

    2nd

--- src/main/java/com/example/plantdex/repository/PlanteRepository.java
+++ src/main/java/com/example/plantdex/repository/PlanteRepository.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.plantdex.model.Categorie;
+import com.example.plantdex.model.Ensoleillement;
 import com.example.plantdex.model.Plante;
 
 public interface PlanteRepository extends JpaRepository<Plante, Long> {
 	Plante findByIdEquals(Long IdExact);
 	List<Plante> findByCategorie(Categorie categorie); 
 	Plante findByNom(String NomExact);
+	Plante findByArrosage(Integer ArrosageExact);
+	Plante findByEnsoleillement(Ensoleillement EnsoleillementExact);
 }
--- src/test/java/com/example/plantdex/PlantdexApplicationTests.java
+++ src/test/java/com/example/plantdex/PlantdexApplicationTests.java
@@ -25,12 +25,13 @@ class PlantdexApplicationTests {
 	}
 	
 	@Test
-	void contextLoads() {
+	void catDeCategorie() {//verifie si categorie specifiée est bien égale à la categorie comparée
 		categorieRepository.save(new Categorie("fleur"));
 		assertEquals("fleur", categorieRepository.findByLibelle("fleur").getLibelle());
+		assertEquals("Fleur", categorieRepository.findByLibelle("fleur").getLibelle());//false car sensible à la casse
 	}	
 	
-	@Test void nbrCat() { 
+	@Test void nbrCat() { //nbr de categorie ds mon repo est bien égal au nbr spécifié
 		categorieRepository.save(new Categorie("sechés"));
 		categorieRepository.save(new Categorie("grasses"));
 		categorieRepository.save(new Categorie("moches"));
@@ -38,21 +39,37 @@ class PlantdexApplicationTests {
 		assertEquals(3, categorieRepository.count());
 	   }
 	 
+	/**
+	 * 
+	 */
 	@Test
-	void contextLoadsPlante() {
-		//Ensoleillement moyenEnsoleillement = Ensoleillement.MOYEN;
-	    Ensoleillement beaucoupEnsoleillement = Ensoleillement.BEAUCOUP;	    
-	    //Ensoleillement peuEnsoleillement = Ensoleillement.PEU;
-		Categorie fleur = new Categorie("fleur");
-		categorieRepository.save(fleur);
-		//Categorie plantesVertes = new Categorie("plantesVertes");
-		//Categorie cactus = new Categorie("cactus");
-		planteRepository.save(new Plante("pivoine", fleur, beaucoupEnsoleillement, 2, "photo pivoine"));
+	void Plante() {
 		
-		assertEquals("pivoine", planteRepository.findByNom("pivoine").getNom());		
+		Ensoleillement moyenEnsoleillement = Ensoleillement.MOYEN;
+	    Ensoleillement beaucoupEnsoleillement = Ensoleillement.BEAUCOUP;	    
+	    Ensoleillement peuEnsoleillement = Ensoleillement.PEU;
+		Categorie fleur = new Categorie("fleur");		
+		Categorie plantesVertes = new Categorie("plantesVertes");
+		Categorie cactus = new Categorie("cactus");
+		categorieRepository.save(fleur);
+		categorieRepository.save(plantesVertes);
+		categorieRepository.save(cactus);
+		
+		//creation d'un objet plante
+		planteRepository.save(new Plante("pivoine", fleur, beaucoupEnsoleillement, 2, "photo pivoine"));				
+		
+		assertEquals("pivoine", planteRepository.findByNom("pivoine").getNom());
+		
+		//false car sensible à la casse
+		/* assertEquals("Pivoine", planteRepository.findByNom("pivoine").getNom()); */
+		
+		//verifier si ma plante a bien cette arrosage et cet ensoleillement
+		assertEquals(2, planteRepository.findByArrosage(2).getArrosage()); 			
+		assertEquals(beaucoupEnsoleillement, planteRepository.findByEnsoleillement(beaucoupEnsoleillement).getEnsoleillement());
+		 
 	}	
 	
-	@Test void nbrPlante() {
+	@Test void nbrPlanteEtCategorie() {
 		Ensoleillement moyenEnsoleillement = Ensoleillement.MOYEN;
 	    Ensoleillement beaucoupEnsoleillement = Ensoleillement.BEAUCOUP;
 	    Ensoleillement peuEnsoleillement = Ensoleillement.PEU;
@@ -64,8 +81,10 @@ class PlantdexApplicationTests {
 		planteRepository.save(new Plante("palmira", plantesVertes, moyenEnsoleillement, 2, "photo palmira"));
 		planteRepository.save(new Plante("phalaenopsys", fleur, beaucoupEnsoleillement, 2, "photo phalaenopsys"));
 		
-		assertEquals(3, planteRepository.count());
+		//nbr plante ds mon repo
+		assertEquals(3, planteRepository.count()); 
+		
+		//nbr plante ds mon repo
+		assertEquals(2, planteRepository.findByCategorie(categorieRepository.findByLibelle("fleur")).size());//nbr plante ds mon repo
 	   }
-	
-
 }
